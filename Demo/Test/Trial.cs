bb04d019420078fd06d62fbc99e9ca2bf127d977
Ullrich Praetz <ullrich@praetz.de>
2022-07-27 14:21:54 +0200

    Demo/Trial - add WebSocket Hub

--- Demo/Test/Trial.cs
+++ Demo/Test/Trial.cs
@@ -60,6 +60,10 @@ namespace DemoTest {
         {
             switch (option) {
                 case "http":    return new HttpClientHub("main_db", "http://localhost:8010/fliox/");
+                case "ws":  // todo simplify
+                    var wsHub = new WebSocketClientHub("main_db", "ws://localhost:8010/fliox/");
+                    wsHub.Connect().Wait();
+                    return wsHub;
                 case "file":    return new FlioxHub(new FileDatabase("main_db", "./DB/main_db"));
                 case "memory":  return new FlioxHub(new MemoryDatabase("main_db"));
             }
